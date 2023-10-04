393d3aa269a813dac260fde9901de27d11d679ba
Priyanshu Rai <95964095+jerry47erf@users.noreply.github.com>
2023-10-04 11:27:23 +0530

    Add files via upload

---
+++ Sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int x;
+    cout<< "Enter First Number :";
+    cin>>x;
+    int y;
+    cout<< "Enter Second Number :";
+    cin>>y;
+    int sum = x + y;
+    cout<< "The Sum of the number is :"<<sum ;// or x +y;
+}
